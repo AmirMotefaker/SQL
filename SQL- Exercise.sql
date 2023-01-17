18f21c9bf3e58102c77bb79310d02802e29f1032
Amir Motefaker <amotef@gmail.com>
2023-01-17 16:01:23 +0330

    Update SQL- Exercise.sql

--- SQL- Exercise.sql
+++ SQL- Exercise.sql
@@ -29,6 +29,25 @@ SELECT column1, column2, ...
 FROM table_name
 WHERE condition;
 
+# SQL statement selects all the customers from the country "Mexico", in the "Customers" table:
+SELECT * FROM Customers
+WHERE Country='Mexico';
+
+# SQL requires single quotes around text values 
+SELECT * FROM Customers
+WHERE CustomerID=1;
+
+# Operators in The WHERE Clause
+-- =	Equal	
+-- >	Greater than	
+-- <	Less than	
+-- >=	Greater than or equal	
+-- <=	Less than or equal	
+-- <>	Not equal. Note: In some versions of SQL this operator may be written as !=	
+-- BETWEEN	Between a certain range	
+-- LIKE	Search for a pattern	
+-- IN	To specify multiple possible values for a column
+
 
 
 
