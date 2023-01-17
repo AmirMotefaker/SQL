72985a64a04afa2aef45506d240721142c0171ef
Amir Motefaker <amotef@gmail.com>
2023-01-17 16:42:37 +0330

    Update SQL- Exercise.sql

--- SQL- Exercise.sql
+++ SQL- Exercise.sql
@@ -111,4 +111,25 @@ WHERE Country='Germany' AND (City='Berlin' OR City='München');
 SELECT * FROM Customers
 WHERE NOT Country='Germany' AND NOT Country='USA';
 
+--------------------------------------------------
+# INSERT INTO Syntax
+# 1. Specify both the column names and the values to be inserted:
+INSERT INTO table_name (column1, column2, column3, ...)
+VALUES (value1, value2, value3, ...);
 
+# 2. If you are adding values for all the columns of the table, you do not need to specify the column names in the SQL query. 
+# However, make sure the order of the values is in the same order as the columns in the table. Here, the INSERT INTO syntax would be as follows:
+INSERT INTO table_name
+VALUES (value1, value2, value3, ...);
+
+# SQL statement inserts a new record in the "Customers" table:
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
+
+
+# Insert Data Only in Specified Columns
+# SQL statement will insert a new record, but only insert data in the "CustomerName", "City", and "Country" columns (CustomerID will be updated automatically):
+INSERT INTO Customers (CustomerName, City, Country)
+VALUES ('Cardinal', 'Stavanger', 'Norway');
+
+-----------------------------------------------------
