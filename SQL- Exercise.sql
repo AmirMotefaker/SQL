4dbfe894cbe50ddf23ceb0094409b213fdd873da
Amir Motefaker <amotef@gmail.com>
2023-01-17 16:05:37 +0330

    Update SQL- Exercise.sql

--- SQL- Exercise.sql
+++ SQL- Exercise.sql
@@ -48,6 +48,41 @@ WHERE CustomerID=1;
 -- LIKE   	Search for a pattern	
 -- IN	      To specify multiple possible values for a column
 
+------------------------------------------
+# AND Syntax
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition1 AND condition2 AND condition3 ...;
 
+# OR Syntax
+SELECT column1, column2, ...
+FROM table_name
+WHERE condition1 OR condition2 OR condition3 ...;
+
+# NOT Syntax
+SELECT column1, column2, ...
+FROM table_name
+WHERE NOT condition;
+
+# SQL statement selects all fields from "Customers" where country is "Germany" AND city is "Berlin":
+SELECT * FROM Customers
+WHERE Country='Germany' AND City='Berlin';
+
+# SQL statement selects all fields from "Customers" where city is "Berlin" OR "München":
+SELECT * FROM Customers
+WHERE City='Berlin' OR City='München';
+
+# SQL statement selects all fields from "Customers" where country is NOT "Germany":
+SELECT * FROM Customers
+WHERE NOT Country='Germany';
+
+# Combining AND, OR and NOT
+# SQL statement selects all fields from "Customers" where country is "Germany" AND city must be "Berlin" OR "München" (use parenthesis to form complex expressions):
+SELECT * FROM Customers
+WHERE Country='Germany' AND (City='Berlin' OR City='München');
+
+# SQL statement selects all fields from "Customers" where country is NOT "Germany" and NOT "USA":
+SELECT * FROM Customers
+WHERE NOT Country='Germany' AND NOT Country='USA';
 
 
