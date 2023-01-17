3b10ddace1b82ca8d79a3a6226ec961bc3b51d1d
Amir Motefaker <amotef@gmail.com>
2023-01-17 16:30:35 +0330

    Update SQL- Exercise.sql

--- SQL- Exercise.sql
+++ SQL- Exercise.sql
@@ -32,6 +32,32 @@ WHERE condition;
 # SQL statement selects all the customers from the country "Mexico", in the "Customers" table:
 SELECT * FROM Customers
 WHERE Country='Mexico';
+-----------------------------------------
+# ORDER BY Syntax
+SELECT column1, column2, ...
+FROM table_name
+ORDER BY column1, column2, ... ASC|DESC;
+
+# SQL statement selects all customers from the "Customers" table, sorted by the "Country" column:
+SELECT * FROM Customers
+ORDER BY Country;
+
+# SQL statement selects all customers from the "Customers" table, sorted DESCENDING by the "Country" column:
+SELECT * FROM Customers
+ORDER BY Country DESC;
+
+# ORDER BY Several Columns Example
+# The following SQL statement selects all customers from the "Customers" table, sorted by the "Country" and the "CustomerName" column.
+# This means that it orders by Country, but if some rows have the same Country, it orders them by CustomerName:
+SELECT * FROM Customers
+ORDER BY Country, CustomerName;
+
+# SQL statement selects all customers from the "Customers" table, sorted ascending by the "Country" and descending by the "CustomerName" column:
+SELECT * FROM Customers
+ORDER BY Country ASC, CustomerName DESC;
+
+----------------------------------------
+
 
 # SQL requires single quotes around text values 
 SELECT * FROM Customers
