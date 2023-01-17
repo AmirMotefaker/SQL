5cc6be8411b9c5516305153cee394f6220725a16
Amir Motefaker <amotef@gmail.com>
2023-01-17 16:02:16 +0330

    Update SQL- Exercise.sql

--- SQL- Exercise.sql
+++ SQL- Exercise.sql
@@ -38,15 +38,15 @@ SELECT * FROM Customers
 WHERE CustomerID=1;
 
 # Operators in The WHERE Clause
--- =	Equal	
--- >	Greater than	
--- <	Less than	
--- >=	Greater than or equal	
--- <=	Less than or equal	
--- <>	Not equal. Note: In some versions of SQL this operator may be written as !=	
+-- =	      Equal	
+-- >       	Greater than	
+-- <	      Less than	
+-- >=	      Greater than or equal	
+-- <=	      Less than or equal	
+-- <>     	Not equal. Note: In some versions of SQL this operator may be written as !=	
 -- BETWEEN	Between a certain range	
--- LIKE	Search for a pattern	
--- IN	To specify multiple possible values for a column
+-- LIKE   	Search for a pattern	
+-- IN	      To specify multiple possible values for a column
 
 
 
