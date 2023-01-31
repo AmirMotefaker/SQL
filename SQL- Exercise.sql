f3ce89588e53d8f749cfbf7147304f937baca23a
Amir Motefaker <amotef@gmail.com>
2023-01-31 10:04:08 +0330

    Update SQL- Exercise.sql

--- SQL- Exercise.sql
+++ SQL- Exercise.sql
@@ -355,3 +355,31 @@ WHERE City LIKE '[!bsp]%';
 
 SELECT * FROM Customers
 WHERE City NOT LIKE '[bsp]%';
+-------------------------------------------
+# SQL IN Operator
+
+# IN Syntax
+SELECT column_name(s)
+FROM table_name
+WHERE column_name IN (value1, value2, ...);
+
+# or:
+
+SELECT column_name(s)
+FROM table_name
+WHERE column_name IN (SELECT STATEMENT);
+
+# SQL statement selects all customers that are located in "Germany", "France" or "UK":
+SELECT * FROM Customers
+WHERE Country IN ('Germany', 'France', 'UK');
+
+# SQL statement selects all customers that are NOT located in "Germany", "France" or "UK":
+SELECT * FROM Customers
+WHERE Country NOT IN ('Germany', 'France', 'UK');
+
+# SQL statement selects all customers that are from the same countries as the suppliers:
+SELECT * FROM Customers
+WHERE Country IN (SELECT Country FROM Suppliers);
+
+------------------------------------------
+
