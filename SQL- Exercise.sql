44e0fa312a6b46536ce4b519824bb35d3626747f
Amir Motefaker <amotef@gmail.com>
2023-01-31 11:44:55 +0330

    Update SQL- Exercise.sql

--- SQL- Exercise.sql
+++ SQL- Exercise.sql
@@ -423,3 +423,13 @@ ORDER BY ProductName;
 SELECT * FROM Products
 WHERE ProductName NOT BETWEEN 'Carnarvon Tigers' AND 'Mozzarella di Giovanni'
 ORDER BY ProductName;
+
+-- BETWEEN Dates
+-- SQL statement selects all orders with an OrderDate between '01-July-1996' and '31-July-1996':
+SELECT * FROM Orders
+WHERE OrderDate BETWEEN #07/01/1996# AND #07/31/1996#;
+--OR:
+SELECT * FROM Orders
+WHERE OrderDate BETWEEN '1996-07-01' AND '1996-07-31';
+
+----------------------------------------------------
