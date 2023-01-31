0c6acbbb675e7cab85c85fca2dd574bad99b7e2e
Amir Motefaker <amotef@gmail.com>
2023-01-31 11:49:04 +0330

    Update SQL- Exercise.sql

--- SQL- Exercise.sql
+++ SQL- Exercise.sql
@@ -12,17 +12,17 @@ SELECT CustomerName, City FROM Customers;
 SELECT * FROM Customers;
 
 -----------------------------------------
-# SELECT DISTINCT Syntax
+-- SELECT DISTINCT Syntax
 SELECT DISTINCT column1, column2, ...
 FROM table_name;
 
-# SELECT Example Without DISTINCT
+-- SELECT Example Without DISTINCT
 SELECT Country FROM Customers;
 
-# SELECT DISTINCT Examples
+-- SELECT DISTINCT Examples
 SELECT DISTINCT Country FROM Customers;
 
-# SQL statement lists the number of different (distinct) customer countries:
+-- SQL statement lists the number of different (distinct) customer countries:
 SELECT COUNT(DISTINCT Country) FROM Customers;
 
 -----------------------------------------
