fbd81a3bc139c213131c52496550f2205441eeaf
Amir Motefaker <amotef@gmail.com>
2023-01-31 10:55:29 +0330

    Update SQL- Exercise.sql

--- SQL- Exercise.sql
+++ SQL- Exercise.sql
@@ -1,12 +1,12 @@
 # W3School SQL Course
 ---------------------------------------
-# SELECT Syntax
+-- SELECT Syntax
 SELECT column1, column2, ...
 FROM table_name;
 
 SELECT * FROM table_name;
 
-# SELECT Column Example
+-- SELECT Column Example
 SELECT CustomerName, City FROM Customers;
 
 SELECT * FROM Customers;
