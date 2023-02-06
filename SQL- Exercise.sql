eebd89cb727ce3b8840913f46a1f0e2f763c8a58
Amir Motefaker <amotef@gmail.com>
2023-02-06 21:23:35 +0330

    Update SQL Aliases

--- SQL- Exercise.sql
+++ SQL- Exercise.sql
@@ -431,3 +431,41 @@ SELECT * FROM Orders
 WHERE OrderDate BETWEEN '1996-07-01' AND '1996-07-31';
 
 ----------------------------------------------------
+-- SQL Aliases
+-- Alias Column Syntax
+SELECT column_name AS alias_name
+FROM table_name;
+
+-- Alias Table Syntax
+SELECT column_name(s)
+FROM table_name AS alias_name;
+
+--Alias for Columns Examples
+-- SQL statement creates two aliases, one for the CustomerID column and one for the CustomerName column:
+SELECT CustomerID AS ID, CustomerName AS Customer
+FROM Customers;
+
+-- SQL statement creates two aliases, one for the CustomerName column and one for the ContactName column. Note: It requires double quotation marks or square brackets if the alias name contains spaces:
+SELECT CustomerName AS Customer, ContactName AS [Contact Person]
+FROM Customers;
+
+-- SQL statement creates an alias named "Address" that combine four columns (Address, PostalCode, City and Country):
+SELECT CustomerName, Address + ', ' + PostalCode + ' ' + City + ', ' + Country AS Address
+FROM Customers;
+
+
+-- SQL statement above to work in MySQL use the following:
+SELECT CustomerName, CONCAT(Address,', ',PostalCode,', ',City,', ',Country) AS Address
+FROM Customers;
+
+
+--Alias for Tables Example
+-- SQL statement selects all the orders from the customer with CustomerID=4 (Around the Horn). We use the "Customers" and "Orders" tables, and give them the table aliases of "c" and "o" respectively (Here we use aliases to make the SQL shorter):
+SELECT o.OrderID, o.OrderDate, c.CustomerName
+FROM Customers AS c, Orders AS o
+WHERE c.CustomerName='Around the Horn' AND c.CustomerID=o.CustomerID;
+
+-- SQL statement is the same as above, but without aliases:
+SELECT Orders.OrderID, Orders.OrderDate, Customers.CustomerName
+FROM Customers, Orders
+WHERE Customers.CustomerName='Around the Horn' AND Customers.CustomerID=Orders.CustomerID;
