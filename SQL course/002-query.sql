679ed34635fd90dbacbbe39934408ddf1511dd7d
Amir Motefaker <amotef@gmail.com>
2023-02-08 14:44:44 +0330

    Update 002-query.sql

--- SQL course/002-query.sql
+++ SQL course/002-query.sql
@@ -13,5 +13,27 @@ WHERE customer_id = 1
 
 SELECT * 
 FROM customers 
+WHERE customer_id = 1
+
+SELECT * 
+FROM customers 
 -- WHERE customer_id = 1
 ORDER BY first_name
+----------------------------------
+SELECT first_name, last_name
+FROM customers
+
+SELECT first_name, last_name, points
+FROM customers
+
+SELECT first_name, last_name, points, points+10
+FROM customers
+
+SELECT first_name, last_name, points, points+10, points * 10 + 100
+FROM customers
+
+SELECT first_name, last_name, points, points+10, points * 10 + 100, (points * 10) + 100
+FROM customers
+
+SELECT first_name, last_name, points, points+10, points * 10 + 100, (points * 10) + 100, points * (10 + 100)
+FROM customers
