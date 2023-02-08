240fe7c6b745cbbdd02a8123cd143b6c3e6989fe
Amir Motefaker <amotef@gmail.com>
2023-02-08 16:07:20 +0330

    Update 002-query.sql

--- SQL course/002-query.sql
+++ SQL course/002-query.sql
@@ -50,4 +50,13 @@ SELECT
     first_name,
     points, (points * 10) + 100 AS 'discount factor'
 FROM customers
------------------------------------
+--------------------------------------------
+SELECT state
+FROM customers
+
+SELECT DISTINCT state
+FROM customers
+    
+SELECT name, unit_price, unit_price * 1.1 AS new_price
+FROM products
+--------------------------------------------- 
