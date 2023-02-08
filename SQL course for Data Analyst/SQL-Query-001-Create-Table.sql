2be1770eb87a3b30ca6a52e2b1b3404204fe1563
Amir Motefaker <amotef@gmail.com>
2023-02-08 16:46:32 +0330

    Added files SQL

---
+++ SQL course for Data Analyst/SQL-Query-001-Create-Table.sql
@@ -0,0 +1,15 @@
+Table 1 Query:
+Create Table EmployeeDemographics 
+(EmployeeID int, 
+FirstName varchar(50), 
+LastName varchar(50), 
+Age int, 
+Gender varchar(50)
+)
+
+Table 2 Query:
+Create Table EmployeeSalary 
+(EmployeeID int, 
+JobTitle varchar(50), 
+Salary int
+)
---
+++ SQL course for Data Analyst/SQL-Query-004-Select.sql
@@ -0,0 +1,11 @@
+SELECT FirstName
+FROM EmployeeDemographics
+
+SELECT FirstName, LastName
+FROM EmployeeDemographics
+
+SELECT *
+FROM EmployeeDemographics
+
+SELECT TOP 5 *
+FROM EmployeeDemographics
