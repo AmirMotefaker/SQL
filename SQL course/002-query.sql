b76bb05733ac038dbaeee6c1798ed5c9ae19a14c
Amir Motefaker <amotef@gmail.com>
2023-02-08 14:31:36 +0330

    Create 002-query.sql

---
+++ SQL course/002-query.sql
@@ -0,0 +1,17 @@
+SELECT * FROM sql_store.customers;
+
+SELECT * FROM sql_store.orders;
+
+-------------------------------------
+USE sql_store;
+
+SELECT * FROM customers
+
+SELECT * 
+FROM customers 
+WHERE customer_id = 1
+
+SELECT * 
+FROM customers 
+-- WHERE customer_id = 1
+ORDER BY first_name
