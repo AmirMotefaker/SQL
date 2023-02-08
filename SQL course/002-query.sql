3b942554075cbf0889144bdeb634e6e0c0b81e82
Amir Motefaker <amotef@gmail.com>
2023-02-08 15:15:58 +0330

    Update 002-query.sql

--- SQL course/002-query.sql
+++ SQL course/002-query.sql
@@ -40,13 +40,13 @@ FROM customers
 
 -----------------------------------
 SELECT 
-	last_name,
+    last_name,
     first_name,
     points, (points * 10) + 100 AS discount_factor
 FROM customers
 
 SELECT 
-	last_name,
+    last_name,
     first_name,
     points, (points * 10) + 100 AS 'discount factor'
 FROM customers
