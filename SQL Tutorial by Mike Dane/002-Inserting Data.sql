4076262731a4560ea27b9c7d639d5d725d8ad012
Amir Motefaker <amotef@gmail.com>
2023-02-15 14:14:22 +0330

    Update 002-Inserting Data.sql

--- SQL Tutorial by Mike Dane/002-Inserting Data.sql
+++ SQL Tutorial by Mike Dane/002-Inserting Data.sql
@@ -1,6 +1,13 @@
 -- Inserting Data
+
 INSERT INTO student VALUES(1, 'Jack', 'Biology');
+
 INSERT INTO student VALUES(2, 'Kate', 'Sociology');
+
 INSERT INTO student(student_id, name) VALUES(3, 'Claire');
+
 INSERT INTO student VALUES(4, 'Jack', 'Biology');
+
 INSERT INTO student VALUES(5, 'Mike', 'Computer Science');
+
+SELECT * FROM student;
