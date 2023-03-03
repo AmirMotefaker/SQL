80e2b4637b4801d924ef96b18adc31488c81b3e7
Amir Motefaker <amotef@gmail.com>
2023-03-03 09:02:14 +0330

    Create 012-Joins.sql

---
+++ SQL Tutorial by Mike Dane/012-Joins.sql
@@ -0,0 +1,10 @@
+-- Joins
+
+
+-- Add the extra branch
+INSERT INTO branch VALUES(4, "Buffalo", NULL, NULL);
+
+SELECT employee.emp_id, employee.first_name, branch.branch_name
+FROM employee
+JOIN branch    -- LEFT JOIN, RIGHT JOIN
+ON employee.emp_id = branch.mgr_id;
